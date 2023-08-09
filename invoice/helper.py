ae3258a87929a5c8798c3df16ae81ecc37618600
mahmoud20212 <occee123@gmail.com>
2023-08-09 03:15:23 +0300

    update

--- invoice/helper.py
+++ invoice/helper.py
@@ -21,10 +21,10 @@ def generate_invoice_pdf(absolute_uri, invoice, *args, **kwargs):
         }
     )
     out = BytesIO()
-    # stylesheets = [
-    #     weasyprint.CSS(os.path.join(settings.STATIC_ROOT, 'css/pdf.css'))
-    # ]
-    weasyprint.HTML(string=html, base_url=absolute_uri).write_pdf(out)
+    stylesheets = [
+        weasyprint.CSS(os.path.join(settings.STATICFILES_DIRS, 'css/pdf.css'))
+    ]
+    weasyprint.HTML(string=html, base_url=absolute_uri).write_pdf(out, stylesheets=stylesheets)
     return out.getvalue()
 
 # old
